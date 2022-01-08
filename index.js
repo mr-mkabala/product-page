5e9be3787920e27ea352fe0d6ad5952ecb4c071a
Mateusz <mat.kabala.91@gmail.com>
2022-01-08 13:36:20 +0100

    Add 1st template for carousel product presentation.

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 //jshint esversion:6
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/:path', (req, res) => {
+  let path = req.params.path;
+  res.render(path);
 });
 
 app.listen(process.env.PORT || 3000, () => {
